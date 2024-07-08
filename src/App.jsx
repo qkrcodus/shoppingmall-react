5b189352e822498d841b35445ccebcda04df1942
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-07-08 11:02:35 +0900

    컴포넌트, 반복문 사용하기 전

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { Button } from 'react-bootstrap';
-import React from 'react'
+import { Container, Navbar ,Nav } from 'react-bootstrap';
+import React, { useState } from 'react'
+import './App.css'
+import data from './data.js'
 
 const App = () => {
+
+  let [shoes]=useState(data)
   return (
     <>
-    <Button variant="primary">Primary</Button>{' '}
-      <Button variant="secondary">Secondary</Button>{' '}
-      <Button variant="success">Success</Button>{' '}
-      <Button variant="warning">Warning</Button>{' '}
-      <Button variant="danger">Danger</Button>{' '}
-      <Button variant="info">Info</Button>{' '}
-      <Button variant="light">Light</Button>{' '}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+    <Navbar bg="light" data-bs-theme="light">
+    <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+    </Container>
+    </Navbar>
+    <div className='main-bg'></div>
+    <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
