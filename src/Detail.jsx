3ff5addfe31a4b28b05a41f17376bc380cb1bb79
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-07-08 18:39:33 +0900

    수정

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -3,13 +3,13 @@ import {Outlet} from 'react-router-dom'
 import {useParams} from 'react-router-dom'
 const Detail = (props) => {
     let {id}=useParams();
-    let 찾는상품=props.shoes.find(function(x){return x.id==id})
+    let 찾는상품=props.shoes.find(function(x){return x.id==(id-1)})
 
   return (
 <div className="container">
   <div className="row">
     <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      <img src={'https://codingapple1.github.io/shop/shoes'+(id)+'.jpg'} width="100%" />
     </div>
     <div className="col-md-6">
       <h4 className="pt-5">{찾는상품.title}</h4>
